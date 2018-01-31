4e57ae9656b6b7cdcfffc4e5ad15ba861511a5db
Jay Jackson <jaynickjackson@gmail.com>
2018-01-31 16:58:56 -0500

    added signup modal

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(()=> {
   $('#test-button').click(() => 
-    $.get('http://localhost:3000/api/test').done(() => console.log('Received Response'))
+    $.get('http://localhost:3000/api/test').done((res) => alert(res))
   )
+
+  $('#signup-form').submit((e) => {
+    e.preventDefault()
+    $.post(
+      'http://localhost:3000/api/user/signup',
+      { 
+        user: {
+          username:$('#signup-input-username').val(), 
+          password:$('#signup-input-password').val()
+        }
+      }
+    ).then(
+      (data) => {console.log(JSON.parse(data))}, 
+      (err) => {console.log(err)}
+    )
+  })
 })
